9e37f569361ef303aa7af9a60454a463293f8c05
arkadiuszgrabowski <37307818+ari0987@users.noreply.github.com>
2018-11-08 20:52:10 +0100

    Test

--- TPAProject/UnitTestProject1/Properties/AssemblyInfo.cs
+++ TPAProject/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("bab2d7f0-040a-4150-82f1-e8201440b0bf")]
+[assembly: Guid("c6e0107d-6213-405e-b302-e1a90e5a6a53")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
