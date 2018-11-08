00091e024fa555f4f54c1a13be93b7019bcc47fa
Kacper Prądzyński <37307285+kacperpradzynski@users.noreply.github.com>
2018-11-08 20:31:11 +0100

    Naprawa testów

--- TPAProject/UnitTests/MVVMUnitTests.cs
+++ TPAProject/UnitTests/MVVMUnitTests.cs
@@ -9,7 +9,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class MVVMUnitTests
     {
         public static TreeViewModel viewModel { get; set; } = new TreeViewModel()
         {
