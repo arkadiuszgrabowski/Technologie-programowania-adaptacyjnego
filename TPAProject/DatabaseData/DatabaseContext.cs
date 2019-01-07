df4a0db7a264d9464737804ae68cdcfabc9f7c06
arkadiuszgrabowski <37307818+ari0987@users.noreply.github.com>
2019-01-07 21:35:36 +0100

    Dodany DatabaseContext

---
+++ TPAProject/DatabaseData/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseData.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseData
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<DatabaseAssembly> AssemblyModel { get; set; }
+        public DbSet<DatabaseMethod> MethodModel { get; set; }
+        public DbSet<DatabaseNamespace> NamespaceModel { get; set; }
+        public DbSet<DatabaseParameter> ParameterModel { get; set; }
+        public DbSet<DatabaseProperty> PropertyModel { get; set; }
+        public DbSet<DatabaseType> TypeModel { get; set; }
+    }
+}
